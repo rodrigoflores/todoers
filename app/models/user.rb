c0befe853d887480e226be275273aadaa34de33e
Rodrigo Flores <rlmflores@gnome.org>
2010-08-18 00:11:00 -0300

    Photo upload working (I have to test it though)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
   
   mount_uploader :photo, PhotoUploader
          
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :photo, :remote_photo_url, :photo_cache, :remove_photo
+
+
 end
