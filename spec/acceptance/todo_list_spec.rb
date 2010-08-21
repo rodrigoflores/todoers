e32a18daebf41f33e817d180c774552cf179ae6e
Rodrigo Flores <rlmflores@gnome.org>
2010-08-21 09:55:56 -0300

    Acceptance for todo-lists

--- spec/acceptance/todo_list_spec.rb
+++ spec/acceptance/todo_list_spec.rb
@@ -1,27 +1,35 @@
 require File.dirname(__FILE__) + '/acceptance_helper'
 
 
-feature "logged in todo-lists!", %q{
+feature "viewing my todo-lists", %q{
   In order to self-organize
   As a user
-  I want to see a list as the owner
+  I want to see my list as the owner
 } do
   
   background do
     @user = Factory(:user, :name => 'Zé', :password => 'pppppp', :password_confirmation => 'pppppp')
     @list = Factory(:todo_list, :name => 'grocery itens', :public => true, :user => @user)
-    visit homepage
     log_in(@user,'pppppp')
+    visit homepage
+  end
+  
+  scenario 'view a list of todo-lists on an index page' do
+    click_link("My page")
+    click_link("My to-do lists")
+    page.should have_content(@list.name)
+    log_out
   end
 
-  scenario 'view a list of todo-lists' do
+  scenario 'view a list of todo-lists on my page' do
     click_link("My page")
     page.should have_content(@list.name)
     log_out
   end
   
   scenario "view a todo-list as the owner" do
-    visit "users/#{@user.id}/todo_lists/#{@list.id}"
+    click_link("My page")
+    click_link(@list.name)
     page.should have_content(@list.name)
     log_out
   end
